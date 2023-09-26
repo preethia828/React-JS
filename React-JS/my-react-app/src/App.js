188dd6c3c85ac88e57f2f5a41f5c665d17bdb7b7
preethia828 <preethia299@gmail.com>
2023-09-26 12:20:30 +0530

    commit msg1

---
+++ React-JS/my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Calculator from "./Usestate/Calculator";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+      {/* <NewUsestate/> */}
+    </>
+  );
+}
+
+export default App;
